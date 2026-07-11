d786fe9b09d6226b8bbe0454f82fe229f5b427ed
Ary Ribeiro <aryribeiro@users.noreply.github.com>
2026-07-10 22:27:14 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.set_page_config(
     page_title="App Live",
     page_icon="📡",
     layout="centered",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="auto"
 )
 
 # CSS customizado
